be83081f46501e06609cf347029a469e115fa665
wlwanpan <wlwanpan@uwaterloo.ca>
2019-05-05 16:18:01 -0700

    Flatten project struct

--- fs/sys/tray.go
+++
@@ -1,4 +0,0 @@
-package sys
-
-// https://dev.to/osuka42/building-a-simple-system-tray-app-with-go-899
-// https://github.com/getlantern/systray
--- fsutil/bytes.go
+++
@@ -1,9 +0,0 @@
-package fsutil
-
-func ToByte(s string) []byte {
-	return []byte(s)
-}
-
-func ToStr(b []byte) string {
-	return string(b[:])
-}
